92fdbfd7ffebd95ead00f0f1912c3d4548a16ed0
Karthic Tantry <karthic.spikers@gmail.com>
2024-08-21 20:40:01 +0530

    Initial commit

---
+++ src/main/java/com/karthic/angelone_integrator_service/AngeloneIntegratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.karthic.angelone_integrator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngeloneIntegratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngeloneIntegratorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthic/angelone_integrator_service/AngeloneIntegratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthic.angelone_integrator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngeloneIntegratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
